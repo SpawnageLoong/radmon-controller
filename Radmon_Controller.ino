d27c4b541e131c0ba7a1c79dfdde187222aed692
Richard Loong <richardloongcj@gmail.com>
2025-02-18 14:21:01 +0800

    Add compile flags for individual code contributions

--- Radmon_Controller.ino
+++ Radmon_Controller.ino
@@ -28,12 +28,19 @@
 // Compile Flags
 //**************************************************************************
 #define DEBUG 1
+//#define SERIAL_MODE 1
+#define CAN_MODE 1
+//#define GM_COUNTER 1
+//#define I2C_SLAVES 1
+//#define TIMER 1
+//#define RTC_ON 1
+
 
 //**************************************************************************
 // Global Variables
 //**************************************************************************
 
-// CAN
+// IO
 char cbuf[100];
 uint8_t iInputChar;
 uint8_t data[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
@@ -44,35 +51,44 @@ volatile int rollingAddress = 0x0000;
 volatile uint32_t gmTubeCount = 0;
 
 // Timer
-Adafruit_ZeroTimer dataTimer = Adafruit_ZeroTimer(DATA_TIMER);
+#ifdef TIMER
+  Adafruit_ZeroTimer dataTimer = Adafruit_ZeroTimer(DATA_TIMER);
+#endif
 
 // RTC
-RTCZero rtc;
+#ifdef RTC_ON
+  RTCZero rtc;
+#endif
 
 // I2C
-int clearBusCounter = 0;
-int handshake1 = 0;
-int handshake2 = 0;
-int errorCount1 = 0;
-int errorCount2 = 0;
-int readResult1 = 0;
-int readResult2 = 0;
-volatile uint8_t slave1_SRAM[ARRAY_SIZE];
-volatile uint8_t slave2_SRAM[ARRAY_SIZE];
+# ifdef I2C_SLAVES
+  int clearBusCounter = 0;
+  int handshake1 = 0;
+  int handshake2 = 0;
+  int errorCount1 = 0;
+  int errorCount2 = 0;
+  int readResult1 = 0;
+  int readResult2 = 0;
+  volatile uint8_t slave1_SRAM[ARRAY_SIZE];
+  volatile uint8_t slave2_SRAM[ARRAY_SIZE];
+#endif
 
 // test vars
 volatile bool togglepin = false;
 
+
 //**************************************************************************
 // Timer Config
 //**************************************************************************
+#ifdef TIMER
+  void TC3_Handler() {
+    Adafruit_ZeroTimer::timerHandler(3);
+  }
+  #define DATA_TIMER_DIVIDER     1024
+  #define DATA_TIMER_PRESCALER   TC_CLOCK_PRESCALER_DIV1024
+  #define DATA_TIMER_COMPARE     (48000000/1024)
+#endif
 
-void TC3_Handler() {
-  Adafruit_ZeroTimer::timerHandler(3);
-}
-#define DATA_TIMER_DIVIDER     1024
-#define DATA_TIMER_PRESCALER   TC_CLOCK_PRESCALER_DIV1024
-#define DATA_TIMER_COMPARE     (48000000/1024)
 
 //**************************************************************************
 // Function Declarations
@@ -95,20 +111,22 @@ int readSram(int slaveAddress);
 
 void setup() {
   // Serial
-  #ifdef DEBUG
+  #ifdef defined(DEBUG) || defined(SERIAL_MODE)
     Serial.begin(9600);
     while (! Serial){}
     Serial.println("\n\n Serial init OK");
   #endif
 
   // CAN
-  CAN.setPins(CAN_PIN_CS, CAN_PIN_INT);
-  CAN.setClockFrequency(8E6);
-  CAN.filter(CAN_REC_ID);
-  CAN.onReceive(canCallback);
-  while (!CAN.begin(500E3)){}
-  #ifdef DEBUG
-    Serial.println("CAN init OK");
+  #ifdef CAN_MODE
+    CAN.setPins(CAN_PIN_CS, CAN_PIN_INT);
+    CAN.setClockFrequency(8E6);
+    CAN.filter(CAN_REC_ID);
+    CAN.onReceive(canCallback);
+    while (!CAN.begin(500E3)){}
+    #ifdef DEBUG
+      Serial.println("CAN init OK");
+    #endif
   #endif
 
   // FRAM
@@ -116,31 +134,39 @@ void setup() {
   digitalWrite(FRAM_CS, HIGH);
 
   // Geiger Counter
-  pinMode(GM_PIN_INT, INPUT_PULLUP);
-  pinMode(GM_PIN_OUT, OUTPUT);
+  #ifdef GM_COUNTER
+    pinMode(GM_PIN_INT, INPUT_PULLUP);
+    pinMode(GM_PIN_OUT, OUTPUT);
+  #endif
 
   // Timer
-  dataTimer.enable(false);
-  dataTimer.configure(DATA_TIMER_PRESCALER,  // prescaler
-        TC_COUNTER_SIZE_16BIT,                // bit width of timer/counter
-        TC_WAVE_GENERATION_MATCH_PWM          // frequency or PWM mode
-  );
-  dataTimer.setCompare(0, DATA_TIMER_COMPARE);
-  dataTimer.setCallback(true, TC_CALLBACK_CC_CHANNEL0, dataTimerCallback);
-  dataTimer.enable(true);
+  #ifdef TIMER
+    dataTimer.enable(false);
+    dataTimer.configure(DATA_TIMER_PRESCALER,  // prescaler
+          TC_COUNTER_SIZE_16BIT,                // bit width of timer/counter
+          TC_WAVE_GENERATION_MATCH_PWM          // frequency or PWM mode
+    );
+    dataTimer.setCompare(0, DATA_TIMER_COMPARE);
+    dataTimer.setCallback(true, TC_CALLBACK_CC_CHANNEL0, dataTimerCallback);
+    dataTimer.enable(true);
+  #endif
 
   // RTC
-  rtc.begin();
+  #ifdef RTC_ON
+    rtc.begin();
+  #endif
 
   // I2C
-  Wire.begin(); // Start I2C as master
-  pinMode(SCL_PIN, INPUT_PULLUP); //for open drain
-  pinMode(SDA_PIN, INPUT_PULLUP); //for open drain
-  pinMode(PCYCLE_PIN, OUTPUT);
-  digitalWrite(PCYCLE_PIN, LOW); //set low to switch on the atmegas
-  pinMode(LOW_PIN, OUTPUT);
-  digitalWrite(LOW_PIN, LOW); //set pin forever low 
-  pinMode(TRIGGER_PIN, INPUT_PULLUP); //set pin high unless external drives it low.
+  #ifdef I2C_SLAVES
+    Wire.begin(); // Start I2C as master
+    pinMode(SCL_PIN, INPUT_PULLUP); //for open drain
+    pinMode(SDA_PIN, INPUT_PULLUP); //for open drain
+    pinMode(PCYCLE_PIN, OUTPUT);
+    digitalWrite(PCYCLE_PIN, LOW); //set low to switch on the atmegas
+    pinMode(LOW_PIN, OUTPUT);
+    digitalWrite(LOW_PIN, LOW); //set pin forever low 
+    pinMode(TRIGGER_PIN, INPUT_PULLUP); //set pin high unless external drives it low.
+  #endif
 
   // Init success
   #ifdef DEBUG
@@ -148,7 +174,9 @@ void setup() {
   #endif
 
   // Attach interrupts
-  attachInterrupt(digitalPinToInterrupt(GM_PIN_INT), gmTubeISR, FALLING);
+  #ifdef GM_COUNTER
+    attachInterrupt(digitalPinToInterrupt(GM_PIN_INT), gmTubeISR, FALLING);
+  #endif
 
   // test setup
   pinMode(LED_BUILTIN, OUTPUT);
@@ -159,79 +187,45 @@ void setup() {
 //**************************************************************************
 
 void loop() {
-  handshake1 = 0;
-  handshake2 = 0;
-  errorCount1 = 0;
-  errorCount2 = 0;
-  readResult1 = 0;
-  readResult2 = 0;
-
-  //int packetSize = CAN.parsePacket();
-  //if (packetSize) {
-  //  canCallback(packetSize);
-  //}
-  /*
-  if (digitalRead(TRIGGER_PIN)){//no trigger do nothing
-    #ifdef DEBUG
-      Serial.println("Read Not Triggered");
-    #endif
-    clearBusCounter = 0;
-    delay(100); //poll ever 100ms
-  }
-
-  else{ //triggered, time to get SRAM data
-    unsigned long start_loop_time = millis();
-    #ifdef DEBUG
-      Serial.println("Reading the 2 SRAMs");
-    #endif  
-
-    #ifdef DEBUG
-      Serial.println("Requesting Handshake from Slave 1");
-    #endif
- 
-    handshake1 = performHandshake(SLAVE1_ADDRESS); //handshake with slave 1
-
-    if (handshake1){ //if handshake 1 is successful
+  #ifdef I2C_SLAVES
+    handshake1 = 0;
+    handshake2 = 0;
+    errorCount1 = 0;
+    errorCount2 = 0;
+    readResult1 = 0;
+    readResult2 = 0;
+    
+    if (digitalRead(TRIGGER_PIN)){//no trigger do nothing
       #ifdef DEBUG
-        Serial.println("Handshake with Slave1 Successful");
-        Serial.println("Reading SRAM of Slave 1");
+        Serial.println("Read Not Triggered");
       #endif
-      readResult1 = readSram(SLAVE1_ADDRESS); //read sram data
+      clearBusCounter = 0;
+      delay(100); //poll ever 100ms
     }
 
-    if ((!handshake1) || (readResult1 != READ_COMPLETE)){ //if no handshake or no read
+    else{ //triggered, time to get SRAM data
+      unsigned long start_loop_time = millis();
       #ifdef DEBUG
-        Serial.println("Need to clear the bus due to slave 1");
-      #endif
+        Serial.println("Reading the 2 SRAMs");
+      #endif  
 
-      if (clearBusCounter >= 1) { //1 clear bus failed
-        clearBusCounter = 0;
-        powerCycle();
-      }
-      else{
-        clearBus();
-        clearBusCounter++;
-      }
-      //then just let the loop() just run again
-    }
-
-    else { //if read1 was done
       #ifdef DEBUG
-        Serial.println("Requesting Handshake from Slave 2");
+        Serial.println("Requesting Handshake from Slave 1");
       #endif
- 
-      handshake2 = performHandshake(SLAVE2_ADDRESS);//try handshake2  
-      if (handshake2){ //read SRAM2 
+  
+      handshake1 = performHandshake(SLAVE1_ADDRESS); //handshake with slave 1
+
+      if (handshake1){ //if handshake 1 is successful
         #ifdef DEBUG
-          Serial.println("Handshake with Slave 2 Successful");
-          Serial.println("Reading SRAM of Slave 2");
+          Serial.println("Handshake with Slave1 Successful");
+          Serial.println("Reading SRAM of Slave 1");
         #endif
-        readResult2 = readSram(SLAVE2_ADDRESS); //read sram data
+        readResult1 = readSram(SLAVE1_ADDRESS); //read sram data
       }
 
-      if ((!handshake2) || (readResult2 != READ_COMPLETE)){ //if either handshake or read wasn't successful.
+      if ((!handshake1) || (readResult1 != READ_COMPLETE)){ //if no handshake or no read
         #ifdef DEBUG
-          Serial.print("Need to clear the bus due to slave 2");
+          Serial.println("Need to clear the bus due to slave 1");
         #endif
 
         if (clearBusCounter >= 1) { //1 clear bus failed
@@ -242,25 +236,55 @@ void loop() {
           clearBus();
           clearBusCounter++;
         }
+        //then just let the loop() just run again
       }
 
-      else { //if read2 was successful
+      else { //if read1 was done
         #ifdef DEBUG
-          Serial.print("Error count for SRAM1: ");
-          Serial.println(errorCount1);
-          Serial.print("Error count for SRAM2: ");
-          Serial.println(errorCount2);
-          Serial.println("Reading of 2 SRAMs successfully completed");
+          Serial.println("Requesting Handshake from Slave 2");
         #endif
+  
+        handshake2 = performHandshake(SLAVE2_ADDRESS);//try handshake2  
+        if (handshake2){ //read SRAM2 
+          #ifdef DEBUG
+            Serial.println("Handshake with Slave 2 Successful");
+            Serial.println("Reading SRAM of Slave 2");
+          #endif
+          readResult2 = readSram(SLAVE2_ADDRESS); //read sram data
+        }
+
+        if ((!handshake2) || (readResult2 != READ_COMPLETE)){ //if either handshake or read wasn't successful.
+          #ifdef DEBUG
+            Serial.print("Need to clear the bus due to slave 2");
+          #endif
+
+          if (clearBusCounter >= 1) { //1 clear bus failed
+            clearBusCounter = 0;
+            powerCycle();
+          }
+          else{
+            clearBus();
+            clearBusCounter++;
+          }
+        }
+
+        else { //if read2 was successful
+          #ifdef DEBUG
+            Serial.print("Error count for SRAM1: ");
+            Serial.println(errorCount1);
+            Serial.print("Error count for SRAM2: ");
+            Serial.println(errorCount2);
+            Serial.println("Reading of 2 SRAMs successfully completed");
+          #endif
+        }
       }
-    }
-    #ifdef DEBUG
-      Serial.print("Time since read trigger is ");
-      Serial.println(millis()-start_loop_time);
-    #endif
-    delay(5000); // Wait before attempting SRAM read again
-  }//trigger_else
-  */
+      #ifdef DEBUG
+        Serial.print("Time since read trigger is ");
+        Serial.println(millis()-start_loop_time);
+      #endif
+      delay(5000); // Wait before attempting SRAM read again
+    }//trigger_else
+  #endif
 } //loop end
 
 //**************************************************************************
@@ -271,89 +295,102 @@ void loop() {
  * \brief           Callback function to handle received CAN packets
  * \details         
  */
-void canCallback(int packetSize) { 
-  uint8_t packetDlc = CAN.packetDlc();
-  uint8_t in_arr[packetDlc];
-  CAN.readBytes(in_arr, packetDlc);
-  #ifdef DEBUG
-    sprintf(cbuf, "Frame data: %X", in_arr[0]);
-    for (int i=1; i<packetDlc; i++) {
-      sprintf(cbuf + strlen(cbuf), ", %X", in_arr[i]);
+#ifdef CAN_MODE
+  void canCallback(int packetSize) { 
+    uint8_t packetDlc = CAN.packetDlc();
+    uint8_t in_arr[packetDlc];
+    CAN.readBytes(in_arr, packetDlc);
+    #ifdef DEBUG
+      sprintf(cbuf, "Frame data: %X", in_arr[0]);
+      for (int i=1; i<packetDlc; i++) {
+        sprintf(cbuf + strlen(cbuf), ", %X", in_arr[i]);
+      }
+      sprintf(cbuf + strlen(cbuf), "\n");
+      Serial.println(cbuf);
+    #endif
+    iInputChar = in_arr[0];
+    uint16_t paramA = 0;
+    uint16_t paramB = 0;
+    uint32_t ts;
+    switch(iInputChar) {
+      case 0x01: // Dump full FRAM data; 32 KBYTE; from 0x0000_0000 to 0x0000_7FFF
+        #ifdef DEBUG
+          Serial.print("dumping 32kB");
+        #endif
+        CAN_Dump_FRAM(0x0000, 0x8000);
+        break;
+      
+      case 0x02: // Dump selected section of FRAM
+        paramA = ((uint16_t)in_arr[1] << 8) | (uint16_t)in_arr[2];
+        paramB = ((uint16_t)in_arr[3] << 8) | (uint16_t)in_arr[4];
+        #ifdef DEBUG
+          sprintf(cbuf, "Dumping %i bytes to CAN", paramB);
+          Serial.print(cbuf);
+        #endif
+        CAN_Dump_FRAM(paramA, paramB);
+        break;
+
+      case 0xAA: // Update RTC
+        #ifdef RTC_ON
+          ts = ((uint32_t)in_arr[1] << 24 | (uint32_t)in_arr[2] << 16 | (uint32_t)in_arr[3] << 8 | (uint32_t)in_arr[4]);
+          rtc.setEpoch(ts);
+          #ifdef DEBUG
+            sprintf(cbuf, "Updated RTC to %i", ts);
+            Serial.print(cbuf);
+          #endif
+        #endif
+        break;
+
+      default: // Unknown command
+        err[1] = 0x01;
+        CAN.beginPacket(CAN_SEND_ID);
+        CAN.write(err, 8);
+        CAN.endPacket();
+        break;
     }
-    sprintf(cbuf + strlen(cbuf), "\n");
-    Serial.println(cbuf);
-  #endif
-  iInputChar = in_arr[0];
-  uint16_t paramA = 0;
-  uint16_t paramB = 0;
-  uint32_t ts;
-  switch(iInputChar) {
-    case 0x01: // Dump full FRAM data; 32 KBYTE; from 0x0000_0000 to 0x0000_7FFF
-      #ifdef DEBUG
-        Serial.print("dumping 32kB");
-      #endif
-      CAN_Dump_FRAM(0x0000, 0x8000);
-      break;
-    
-    case 0x02: // Dump selected section of FRAM
-      paramA = ((uint16_t)in_arr[1] << 8) | (uint16_t)in_arr[2];
-      paramB = ((uint16_t)in_arr[3] << 8) | (uint16_t)in_arr[4];
-      #ifdef DEBUG
-        sprintf(cbuf, "Dumping %i bytes to CAN", paramB);
-        Serial.print(cbuf);
-      #endif
-      CAN_Dump_FRAM(paramA, paramB);
-      break;
-
-    case 0xAA: // Update RTC
-      ts = ((uint32_t)in_arr[1] << 24 | (uint32_t)in_arr[2] << 16 | (uint32_t)in_arr[3] << 8 | (uint32_t)in_arr[4]);
-      rtc.setEpoch(ts);
-      #ifdef DEBUG
-        sprintf(cbuf, "Updated RTC to %i", ts);
-        Serial.print(cbuf);
-      #endif
-      break;
-
-    default: // Unknown command
-      err[1] = 0x01;
-      CAN.beginPacket(CAN_SEND_ID);
-      CAN.write(err, 8);
-      CAN.endPacket();
-      break;
+    iInputChar = NULL;
   }
-  iInputChar = NULL;
-}
+#endif
 
 
 /**
  * \brief           Callback function to handle signals from the Geiger-Muller Tube
  * \details         Increments gmTubeCount when triggered.
  */
-void gmTubeISR() {
-  gmTubeCount++;  // Increment count each time a falling edge is detected
-}
+#ifdef GM_COUNTER
+  void gmTubeISR() {
+    gmTubeCount++;  // Increment count each time a falling edge is detected
+  }
+#endif
 
 
 /**
  * \brief           Callback function to handle dataTimer compare interrupts
  * \details         Updates FRAM and resets current GM Tube count
  */
-void dataTimerCallback(void) {
-  uint32_t now = rtc.getEpoch();
-  uint16_t errCountA = 0;
-  uint16_t errCountB = 0;
-  uint32_t errCountMerged = ((uint32_t)errCountA << 16 | (uint32_t)errCountB);
-  int fixedFrame[] = { now, gmTubeCount, errCountMerged };
-  //uint32_t testDataA[64];
-  //uint32_t testDataB[64];
-  Store_Data_To_FRAM(rollingAddress, fixedFrame, sizeof(fixedFrame));
-  rollingAddress += 32; // temp value, 8 bytes
-  digitalWrite(LED_BUILTIN, togglepin);
-  togglepin = !togglepin;
-  gmTubeCount = 0;
-}
+#ifdef defined(TIMER) && defined(GM_COUNTER)
+  void dataTimerCallback(void) {
+    #ifdef RTC_ON
+      uint32_t now = rtc.getEpoch();
+    #else
+      uint32_t now = 0;
+    #endif
+    uint16_t errCountA = 0;
+    uint16_t errCountB = 0;
+    uint32_t errCountMerged = ((uint32_t)errCountA << 16 | (uint32_t)errCountB);
+    int fixedFrame[] = { now, gmTubeCount, errCountMerged };
+    //uint32_t testDataA[64];
+    //uint32_t testDataB[64];
+    Store_Data_To_FRAM(rollingAddress, fixedFrame, sizeof(fixedFrame));
+    rollingAddress += 32; // temp value, 8 bytes
+    digitalWrite(LED_BUILTIN, togglepin);
+    togglepin = !togglepin;
+    gmTubeCount = 0;
+  }
+#endif
 
 
+#ifdef I2_SLAVES
 /**
  * \brief           Clears the I2C bus after a lockup
  * \details         
@@ -542,3 +579,4 @@ int readSram(int slaveAddress){
   #endif
   return TIMEOUT;
 }
+#endif
