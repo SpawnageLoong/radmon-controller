534e1d8e893dec1f0a552d1d780901a634567109
Richard Loong <richardloongcj@gmail.com>
2025-03-24 16:01:38 +0800

    Remove debounce timer implementation

--- Radmon_Controller.ino
+++ Radmon_Controller.ino
@@ -34,8 +34,7 @@
 #define GM_COUNTER 1
 #define I2C_SLAVES 1
 #define TIMER 1
-//define TIMER_LED 1
-#define DEBOUNCE_LED 1
+#define TIMER_LED 1
 #define GM_DEBUG_SERIAL 1
 #define RTC_ON 1
 
@@ -60,8 +59,6 @@ bool is_FRAM_full = false;
 
 // GM Tube
 volatile uint32_t gmTubeCount = 0;
-volatile bool is_debouncer_active = false;
-Adafruit_ZeroTimer debounceTimer = Adafruit_ZeroTimer(DEBOUNCE_TIMER);
 
 // Timer
 #ifdef TIMER
@@ -104,13 +101,6 @@ volatile bool togglepin = false;
   #define DATA_TIMER_COMPARE     (48000000/1024)            // 1 second
 #endif
 
-void TC4_Handler() {
-  Adafruit_ZeroTimer::timerHandler(4);
-}
-#define DEBOUNCE_TIMER_DIVIDER     1024
-#define DEBOUNCE_TIMER_PRESCALER   TC_CLOCK_PRESCALER_DIV1024
-#define DEBOUNCE_TIMER_COMPARE     (48000000/5120000)         // 200 microsec
-
 
 //**************************************************************************
 // Function Declarations
@@ -149,13 +139,6 @@ void setup() {
   #ifdef GM_COUNTER
     pinMode(GM_PIN_INT, INPUT_PULLUP);
     pinMode(GM_PIN_OUT, OUTPUT);
-    debounceTimer.enable(false);
-    debounceTimer.configure(DEBOUNCE_TIMER_PRESCALER,  // 48MHz clock divided by 1024 = 46.875kHz
-          TC_COUNTER_SIZE_16BIT,                // 65,536 bit width of counter
-          TC_WAVE_GENERATION_MATCH_PWM          // frequency or PWM mode
-    );
-    debounceTimer.setCompare(0, DEBOUNCE_TIMER_COMPARE);
-    debounceTimer.setCallback(true, TC_CALLBACK_CC_CHANNEL0, debounceTimerCallback);
   #endif
 
   // Builtin LED for 1-sec timer or debouncer
@@ -413,39 +396,14 @@ void executeCmd() {
   */
   void gmTubeISR() {
     #ifdef GM_DEBUG_SERIAL
-      Serial.print("GM interrupt. is_debouncer_active = ");
-      Serial.println(is_debouncer_active);
+      Serial.println("GM interrupt.");
     #endif
-    if (is_debouncer_active == true) {
-      return;
-    }
     gmTubeCount++;  // Increment count each time a falling edge is detected
     #ifdef GM_DEBUG_SERIAL
       Serial.println("GM Count incremented.");
     #endif
-    is_debouncer_active = true; // Activate the debouncer
-    debounceTimer.enable(true); // Start the debounce timer
     #ifdef DEBUG
       digitalWrite(GM_PIN_OUT, HIGH);
-    #endif
-    #ifdef DEBOUNCE_LED
-      digitalWrite(LED_BUILTIN, HIGH);
-    #endif
-  }
-
-
-    /**
-  * \brief           Callback function to handle debounceTimer compare interrupts
-  * \details         Sets debouncer flag to false and deactivates the debouncer timer.
-  */
-  void debounceTimerCallback() {
-    is_debouncer_active = false;
-    debounceTimer.enable(false);
-    #ifdef DEBOUNCE_LED
-      digitalWrite(LED_BUILTIN, LOW);
-    #endif
-    #ifdef GM_DEBUG_SERIAL
-      Serial.println("Debouncer deactivated.");
     #endif
   }
 
